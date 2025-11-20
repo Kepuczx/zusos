18f0301fac382a881bbb2c6174909bd5db55b8c1
Kepuczx <138998221+Kepuczx@users.noreply.github.com>
2025-11-20 12:33:26 +0100

    Add files via upload

---
+++ scripts/nav-click.js
@@ -0,0 +1,30 @@
+const nav_links = document.querySelector(".nav-links");
+const hamburger = document.querySelector(".hamburger");
+
+const avatar_links = document.querySelector(".avatar-links");
+const avatar = document.querySelector(".avatar");
+
+// pokaz/ukryj menu hamburgera
+hamburger.onclick = function(event) {
+  event.stopPropagation(); // zatrzymuje kliknięcie, żeby nie zamknęło menu od razu
+  nav_links.style.display = 
+    nav_links.style.display === "block" ? "none" : "block";
+};
+
+// pokaz/ukryj menu avatara
+avatar.onclick = function(event) {
+  event.stopPropagation();
+  avatar_links.style.display = 
+    avatar_links.style.display === "block" ? "none" : "block";
+    nav_links.style.display === "none";
+};
+
+// kliknięcie poza menu — ukryj oba
+document.addEventListener("click", (event) => {
+  if (!nav_links.contains(event.target) && event.target !== hamburger) {
+    nav_links.style.display = "none";
+  }
+  if (!avatar_links.contains(event.target) && event.target !== avatar) {
+    avatar_links.style.display = "none";
+  }
+});
